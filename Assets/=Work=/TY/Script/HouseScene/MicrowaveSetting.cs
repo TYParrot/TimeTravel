f61aefe2add201c09dfe93d3675686c024dbac66
TYParrot <73172379+TYParrot@users.noreply.github.com>
2024-05-18 22:48:21 +0900

    [FEAT]하우스씬 오브젝트 애니메이션 추가, Light 추가, xr rig 세팅 추가 [STYLE]Start,Prologue,House씬 UI 최적화

---
+++ Assets/=Work=/TY/Script/HouseScene/MicrowaveSetting.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//해당 오브젝트의 클리어 여부에 따라서 텍스트 비활성화 혹은 클리어 표시가 추가 될 수 있습니다.
+public class MicrowaveSetting : MonoBehaviour
+{
+    public GameObject TextCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(Managers.Game.ReturnMicrowave()){
+            TextCanvas.SetActive(false);
+        }
+    }
+
+}
