002090236f133aa1e769fafb02e4ab06ced19b10
taeyoon <taeyoon0305@hanyang.ac.kr>
2024-07-27 15:52:08 +0900

    매니저 수정

--- Assets/=Work=/TY/Script/Manager/Managers.cs
+++ Assets/=Work=/TY/Script/Manager/Managers.cs
@@ -5,10 +5,6 @@ using Game;
 using Game.Helpers;
 using Unity.VisualScripting;
 
-//Managers에서 GameManager로 접근해야함. 
-//추후 oculus 연결 시에 xrSimulator 없애기.
-//현재 Start씬에 xr을 놓은 상태라 추후에 0으로 씬전환시 오디오 중복 문제 있음.
-//Managers에서 Start시에 Player 없으면 Oculus 프리팹 Player이름으로 생성되도록 하는 방식으로 변경해야함.
 public class Managers : Game.Helpers.Singleton<Managers>
 {
     private GameManager _game = new();
