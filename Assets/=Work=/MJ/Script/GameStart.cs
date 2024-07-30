fe56baa6fda9383344e6fbb55bf854dd5070e233
TYParrot <taeyoon0305@hanyang.ac.kr>
2024-07-30 15:11:47 +0900

    [FEAT]하우스 씬 조명 수정 및 설명서 업데이트

--- Assets/=Work=/MJ/Script/GameStart.cs
+++ Assets/=Work=/MJ/Script/GameStart.cs
@@ -5,6 +5,6 @@ public class GameStart : MonoBehaviour
     void Start()
     {
         // 게임 오브젝트의 위치를 (0, 0.61, 1)로 설정
-        transform.position = new Vector3(0, 0.61f, 1);
+        transform.position = new Vector3(0, 2.15f, 1);
     }
 }
